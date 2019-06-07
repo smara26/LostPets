35d28cad43fdbd82eea29c7c716fd07978249ef8
georgianaet <georgianaelenatrofin@gmail.com>
2019-06-07 10:34:14 +0300

    fixed

--- Back/login.php
+++ Back/login.php
@@ -3,14 +3,16 @@
 if (isset($_POST['loginEmail']) && isset($_POST['loginPassword'])) {
     $uname = $_POST['loginEmail'];
     $pass = $_POST['loginPassword'];
+
     if (!filter_var($uname, FILTER_VALIDATE_EMAIL) || strlen($pass) < 6) {
         echo "<script>alert('Username or password is invalid!')</script>";
-        echo "<script>location.href='login.php'</script>";
+        echo "<script>location.href='../Front/login/login.html'</script>";
     } else {
+
         session_start();
         if (isset($_SESSION['uname'])) {
             $conn = mysqli_connect("localhost", "root", "", "lost_pets");
-            $name = "SELECT lastName,firstName from user WHERE email=? AND password=?";
+            $name = "SELECT lastName,firstName from users WHERE email=? AND password=?";
 
             $stmt2 = $conn->prepare($name);
             $stmt2->bind_param("ss", $uname, $pass);
@@ -23,10 +25,10 @@ if (isset($_POST['loginEmail']) && isset($_POST['loginPassword'])) {
                 $_SESSION['uname'] = $uname;
 //                header("location:welcome.php");
                 echo "<p>Welcome, " . $firstName . " " . $lastName . " !</p>";
-                echo "<a href='../home/home.html'><input type='button' value='Log out' name='logout'></a>";
+                echo "<a href='../Front/home/home.html'><input type='button' value='Log out' name='logout'></a>";
             } else {
                 echo "<script>alert('Username or password is invalid!')</script>";
-                echo "<script>location.href='login.html'</script>";
+                echo "<script>location.href='../Front/login/login.html'</script>";
             }
             mysqli_close($conn);
         }
--- Back/register.php
+++ Back/register.php
@@ -7,12 +7,12 @@ if (!empty($_POST['registerFName']) && !empty($_POST['registerLName']) && !empty
 
     if (!filter_var($uname, FILTER_VALIDATE_EMAIL) || ($_POST['registerPassword'] !== $_POST['registerRepeatPassword'])) {
         echo "<script>alert('Username or password is invalid!')</script>";
-        echo "<script>location.href='register.html'</script>";
+        echo "<script>location.href='../Front/register/register.html'</script>";
 
     }
     $conn = mysqli_connect('localhost', 'root', '', 'lost_pets');
 
-    $user_check_email = "SELECT count(email) FROM user WHERE email=?";
+    $user_check_email = "SELECT count(email) FROM users WHERE email=?";
 
     $stmt2 = $conn->prepare($user_check_email);
     $stmt2->bind_param("s", $uname);
@@ -30,15 +30,16 @@ if (!empty($_POST['registerFName']) && !empty($_POST['registerLName']) && !empty
         $pass=$_POST['registerPassword'];
         $gender=$_POST['registerGender'];
         $age=$_POST['registerAge'];
-        $user_register = "INSERT INTO user (email, password, firstName,lastName,gender,age) VALUES (?,?,?,?,?,?);";
+
+        $user_register = "INSERT INTO users (email, password, firstName,lastName,gender,age) VALUES (?,?,?,?,?,?);";
 
         $stmt2 = $conn->prepare($user_register);
         $stmt2->bind_param("sssssi", $uname,$pass,$fName,$lName,$gender,$age);
         $stmt2->execute();
-        echo "<script>location.href='../login/login.html'</script>";
+        echo "<script>location.href='../Front/login/login.html'</script>";
 
     }
 
 }
-
+mysqli_close($conn);
 ?>
